68505a6c124cfd1c981c120142a436b13e0b6655
chandana-sree-krishna <chandanashreekrishna@gmail.com>
2024-08-31 23:31:02 -0400

    added home, menu, cart

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            In addition to the delicious assortment of cupcakes & cakes we have
+            available in the shop daily, we also do custom orders. Get in touch
+            today to place your order!
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Right Reseved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
