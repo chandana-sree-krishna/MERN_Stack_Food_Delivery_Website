5b49b177fc00189f84f36fe4f37c84600e34fd3a
chandana-sree-krishna <chandanashreekrishna@gmail.com>
2024-09-03 20:28:49 -0400

    added db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://chandanasreekrishna:<db_password>@cluster0.yhz0l.mongodb.net/food-del"
+      "mongodb+srv://chandanasreekrishna:Chandana1998!@cluster0.yhz0l.mongodb.net/food-del"
     )
     .then(() => console.log("DB Connected"));
 };
